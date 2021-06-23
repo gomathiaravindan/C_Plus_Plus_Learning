a7bb8305844077c79ba81edca8c4c505a665e816
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-06-23 21:26:57 +0530

    Add files via upload

---
+++ C++_3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter two numbers:";
+	cin>>a>>b;
+	cout<<"The sum of two numbers:"<<a+b<<endl;
+	cout<<"The difference is:"<<a-b<<endl;
+	cout<<"The product is:"<<a * b<<endl;
+	cout<<"The division is:"<<a / b<<endl;
+	cout<<"The modulus is:"<<a % b<<endl;
+	cout<<"The AND value is:"<<(a & b)<<endl;
+	cout<<"The OR value is:"<<(a | b)<<endl;
+	cout<<"The XOR value is:"<<(a ^ b)<<endl;
+	cout<<"The post increament value is:"<<a++<<endl;
+	cout<<"the post decreament value is:"<<a--<<endl;
+	cout<<"The pre increament value is:"<<++a<<endl;
+	cout<<"The pre decreament value is:"<<--a<<endl;
+}
