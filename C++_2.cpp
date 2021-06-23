5f1a41d6f9ef39d490617753ab0dc49e106496d2
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-06-23 21:26:34 +0530

    Add files via upload

---
+++ C++_2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main()
+{
+	int a;
+	char ch;
+	std::cout<<"Enter an intger:";
+	std::cin>>a;
+	std::cout<<"Enter a character:";
+	std::cin>>ch;
+	std::cout<<"The value of a is:"<<a << std::endl;
+	std::cout<<"The character ch is:"<<sizeof(ch);
+	return 0;
+}
